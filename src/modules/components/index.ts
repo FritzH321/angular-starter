a6169a696612bf9bef8cb8af74169e4c869a3c35
Fritz Hoste <hoste.fritz@gmail.com>
2016-05-09 10:51:59 +0200

    remove component

--- src/modules/components/index.ts
+++ src/modules/components/index.ts
@@ -1,9 +1,7 @@
 import IModule = angular.IModule;
 
 import {MainApplicationComponent} from './main-application/main-application.component';
-import {EmbedJSComponent} from './embedjs/embedjs.component';
 
 
 export var mod: IModule = angular.module('app.component', [])
  .component("mainApplication", new MainApplicationComponent())
- .component("embedjs", new EmbedJSComponent());
